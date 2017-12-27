1b3842a94902aeb18391a1a14cacc24969754712
markl <1119072529@qq.com>
2017-12-27 17:26:50 +0800

    添加连接注释

--- tfRnnTest1.py
+++ tfRnnTest1.py
@@ -3,7 +3,10 @@
 # Auther    : Mark
 # File      :
 # Date      :
-# Des       : http://blog.csdn.net/jerr__y/article/details/61195257
+# Des       :
+    工程介绍:http://blog.csdn.net/jerr__y/article/details/61195257
+    TensorFlow命令介绍:https://wenku.baidu.com/view/7b416358a9114431b90d6c85ec3a87c240288ac1.html
+
 '''
 
 import tensorflow as tf
@@ -97,9 +100,38 @@ with tf.variable_scope('RNN'):
 h_state = outputs[-1]
 print(h_state)
 
-if __name__ == '__main__':
-    configGPU()
-    importData()
+# 上面 LSTM 部分的输出会是一个 [hidden_size] 的tensor，我们要分类的话，还需要接一个 softmax 层
+# 首先定义 softmax 的连接权重矩阵和偏置
+# out_W = tf.placeholder(tf.float32, [hidden_size, class_num], name='out_Weights')
+# out_bias = tf.placeholder(tf.float32, [class_num], name='out_bias')
+# 开始训练和测试
+W = tf.Variable(tf.truncated_normal([hidden_size, class_num], stddev=0.1), dtype=tf.float32)
+bias = tf.Variable(tf.constant(0.1,shape=[class_num]), dtype=tf.float32)
+y_pre = tf.nn.softmax(tf.matmul(h_state, W) + bias)
+
+
+# 损失和评估函数
+cross_entropy = -tf.reduce_mean(y * tf.log(y_pre))
+train_op = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.argmax(y_pre,1), tf.argmax(y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+
+sess.run(tf.global_variables_initializer())
+for i in range(2000):
+    _batch_size = 128
+    batch = mnist.train.next_batch(_batch_size)
+    if (i+1)%200 == 0:
+        train_accuracy = sess.run(accuracy, feed_dict={
+            _X:batch[0], y: batch[1], keep_prob: 1.0, batch_size: _batch_size})
+        # 已经迭代完成的 epoch 数: mnist.train.epochs_completed
+        print "Iter%d, step %d, training accuracy %g" % ( mnist.train.epochs_completed, (i+1), train_accuracy)
+    sess.run(train_op, feed_dict={_X: batch[0], y: batch[1], keep_prob: 0.5, batch_size: _batch_size})
+
+# 计算测试数据的准确率
+print "test accuracy %g"% sess.run(accuracy, feed_dict={
+    _X: mnist.test.images, y: mnist.test.labels, keep_prob: 1.0, batch_size:mnist.test.images.shape[0]})
 
 
 
